bf15e329b1f0bab028708f175b514cbb92197fc9
tabudongwodelei <617435300@qq.com>
2017-12-27 09:08:21 +0800

    第一次

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lenovo on 17/12/26 026.
+ */
+var swiper = new Swiper('.lun1', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
